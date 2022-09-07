f3a232499040d30dca58b60cb3caa09366d21245
santiago <santiagoneira52@gmail.com>
2022-09-07 12:12:32 -0300

    conection succes with the database

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 // importo los paquetes que voy a usar (instalados)
+const { table } = require("console");
 const express = require("express");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 
 // acceso a metodos y funciones de express
 app = express();
+
+// SET conexión a la base de datos
+const conexion = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "empresa-x",
+  port: "3307",
+});
 
 // ENV recibido por parámetro
 const PUERTO = process.env["PUERTO"] || 3000;
@@ -14,3 +24,20 @@ app.listen(PUERTO, () =>
 
 // endpoints de nuestra app [ HOME ]
 app.get("/", (req, res) => res.send("Home de la app"));
+
+// traer todos los empleados
+app.get("/empleados", (req, res) => {
+  conexion.query("SELECT * FROM empleados", (error, filas) => {
+    if (error) console.log(error);
+    else res.send(filas);
+  });
+});
+
+// crear empleados
+app.post("");
+
+// TEST conexión a la base de datos
+conexion.connect(function (error) {
+  if (error) console.log("Error en la conexión", error);
+  else console.log("Conexion exitosa con DB!");
+});
